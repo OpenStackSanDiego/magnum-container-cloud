2db18793908df2cc72bc565272cfc44a4163d77b
johnstudarus <john@jhlconsulting.com>
2017-07-15 11:09:15 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,14 +21,14 @@ IMG_NAME=Fedora-Atomic-25
 OS_DISTRO=fedora-atomic
 wget -q -O - $IMG_URL | \
 glance  --os-image-api-version 2 image-create --protected True --name $IMG_NAME \
-        --visibility public --disk-format raw --container-format bare --property os_distro='$OS_DISTRO' --progress
+        --visibility public --disk-format raw --container-format bare --property os_distro=$OS_DISTRO --progress
         
 IMG_URL=https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2
 IMG_NAME=CentOS-7
 OS_DISTRO=centos
 wget -q -O - $IMG_URL | \
 glance  --os-image-api-version 2 image-create --protected True --name $IMG_NAME \
-        --visibility public --disk-format raw --container-format bare --property os_distro='$OS_DISTRO' --progress
+        --visibility public --disk-format raw --container-format bare --property os_distro=$OS_DISTRO --progress
 
 wget https://stable.release.core-os.net/amd64-usr/current/coreos_production_openstack_image.img.bz2
 bunzip2 coreos_production_openstack_image.img.bz2
@@ -37,14 +37,14 @@ IMG_NAME=Container-Linux
 OS_DISTRO=coreos
 wget -q -O - $IMG_URL | \
 glance  --os-image-api-version 2 image-create --protected True --name $IMG_NAME \
-        --visibility public --disk-format raw --container-format bare --property os_distro='$OS_DISTRO' --progress
+        --visibility public --disk-format raw --container-format bare --property os_distro=$OS_DISTRO --progress
         
 IMG_URL=http://download.cirros-cloud.net/0.3.5/cirros-0.3.5-x86_64-disk.img
 IMG_NAME=CirrOS-0.3.5
 OS_DISTRO=cirros
 wget -q -O - $IMG_URL | \
 glance  --os-image-api-version 2 image-create --protected True --name $IMG_NAME \
-        --visibility public --disk-format raw --container-format bare --property os_distro='$OS_DISTRO' --progress        
+        --visibility public --disk-format raw --container-format bare --property os_distro=$OS_DISTRO --progress        
 
 
   
